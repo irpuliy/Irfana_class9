b11bb9a909ebde5b5c545f9c2b5f38f3bedabff0
irpuliy <87345695+irpuliy@users.noreply.github.com>
2021-07-12 20:38:50 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,16 @@
+var ball;
+function setup() {
+  createCanvas(400,400);
+  ball = createSprite(100,100,20,20);
+  ball.shapeColor = "black";
+}
+
+function draw() 
+{
+  background("blue");
+drawSprites();
+}
+
+
+
+
